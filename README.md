0cd4ae325181c220a1e33639178b0b2656ad703d
maroc81 <maroc81@users.noreply.github.com>
2016-08-22 13:59:03 -0500

    Completed initial version of README and juniper-vpn.sh script

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# juniper-vpn
-Linux tools for managing juniper vpn connections
+# Linux Juniper VPN Tools
+## Background
+The Linux version of the juniper vpn client has a number of problems including
+
+* Frequent disconnects
+* Connection stops passing data
+* Inablity to recover from network changes (e.g. wifi drops out, resume from standby)
+
+When any of the above happen, the user must go back to the browser and restart the client.
+
+The tools here make using the juniper vpn client on linux a little less painless by managing the connection automatically.   It will do things like 
+
+* Detect network interruption, disconnect the client, and restart client when network is restored
+* Detect connection is up but no longer passing data and restart the connection
+* Allow user to easily disconnect and reconnect 
+
+The tools here are based on the work done by Scott: http://makefile.com/.plan/2009/10/juniper-vpn-64-bit-linux-an-unsolved-mystery/
+
+He figured out how to make the ncui library an executable binary and how to launch it from the command line to connect to the juniper/pulse vpn.
+
+## Setup
+
+1. Access your juniper vpn site using a browser to install the juniper client and necessary files
+2. Install gcc-multilib packages for your Linux distribution
+3. Install sqlite3
+4. Download the juniper-vpn.sh script
+5. Edit the script and change the vpn site and path to your cookies database (currently only reading firefox cookies is implemented)
+
+## Using
+In order to use the script you must login through the vpn web interface first.  This will authenticate you with the vpn and provide the DSID cookie.  It will also allow the vpn site to run utilities such as host checkers that some companies require.
+
+1. Login to the vpn site and close the juniper client if it launches
+2. Run the juniper-vpn.sh script. For now, this requires a dedicated terminal to interact with the script.
+3. While running:
+	* Hit 'q' to disconnect and exit
+	* Hit 'd' to disconnect and wait
+	* Hit 'r' to reconnect the vpn
+
+Note: only basic testing has been done so expect to find some issues.
+
+## Future
+* Python qt GUI version with connect/disconnect buttons and status information
+* Tray icon to allow disconnect/reconnect
+* Add support for additonal browser cookies
+* Add thin client browser to GUI version allowing login and connection management from one program
+
+
+
+
+
+
---
+++ juniper-vpn.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+#set -x
+
+VPNSITE="vpn.site.com"
+JUNIPER="$HOME/.juniper_networks/"
+COOKIES="$HOME/.mozilla/firefox/mwad0hks.default/cookies.sqlite"
+NCUI="$JUNIPER/network_connect/ncui"
+CERT="$JUNIPER/network_connect/ssl.crt"
+
+
+function getCert()
+{
+    [ -r "$CERT" ] && return
+    openssl s_client -connect $VPNSITE 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | openssl x509 -outform der > ${CERT}
+    if [ ! -r "$CERT" ]; then
+	echo "Couldn't get cert from vpn site $VPNSITE"
+	exit 1
+    fi
+}
+
+function getDsid()
+{
+    if [ ! -r "$COOKIES" ]
+    then
+	echo "Couldn't read browser cookie cache $COOKIES"
+	echo "Make sure you set the correct path in this script"
+	exit 1
+    fi
+    
+    DSID=$(sqlite3 "$COOKIES"  "select value from moz_cookies where name='DSID'")
+    if [ -z "$DSID" ] 
+    then
+	echo "Couldn't get DSID, you must login to $VPNSITE from your browser"
+	echo "and the sqlite3 package must be installed on your system"
+	return 1
+    fi
+    return 0
+}
+
+function makeNcui()
+{
+    [ -x ${NCUI} ] && return
+    gcc -m32 -Wl,-rpath,${JUNIPER}/network_connect/ -o ${NCUI} ${JUNIPER}/network_connect/libncui.so
+    if [ ! -x ${NCUI} ]
+    then
+	echo "Couldn't make ncui executable"
+	echo "Make sure you have gcc-multilib packages installed"
+	exit 1
+    fi
+    echo "Setting NCUI executable set UID root"
+    echo "Enter password for sudo"
+    sudo chown root:root $NCUI || exit 1
+    sudo chmod 4755 $NCUI || exit 1
+}
+
+function connect()
+{
+    getDsid || return 1
+    # echo new line in order to pass enter to Password prompt
+    (echo | $NCUI -h $VPNSITE -c DSID=$DSID -f ${CERT}  >/dev/null 2>&1 ) 2>/dev/null &
+}
+
+function disconnect()
+{
+    pgrep ncui >/dev/null || return
+    for i in "1 2 3 4 5"
+    do
+	    pgrep ncui >/dev/null || break
+	    killall ncui
+	    sleep 1
+    done
+    pgrep ncui >/dev/null && killall -9 ncui
+}
+
+function restoreResolv()
+{
+    # killing/exiting ncui in disconnect should restore resolv.conf
+    # if it does not, this will restore it
+    # TODO: eventually call this before quitting or after a 'd' command
+    if [ ! -L /etc/resolve.conf ] && [ -f /run/resolvconf/resolv.conf ]
+    then
+	sudo rm -f /etc/resolv.conf
+	sudo ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
+	echo "Restored /etc/resolv.conf"
+    fi
+}
+
+function testNetwork()
+{
+    # ping default gateway since some networks like airport wifi 
+    # block pings outside the network, being able to ping the gateway
+    # doesn't mean network is good however, may need to use wget
+    # to retreive public url of some minimal web page
+    GATEWAY=$(ip route | grep default | grep -v tun | awk '{print $3}')
+    ping -c 1 -W 2 $GATEWAY >/dev/null 2>&1 
+    if [ "$?" -ne 0 ]; then
+	return 1
+    fi
+    return 0
+}
+
+function testVpn()
+{
+    # ping the vpn gateway to see if our vpn connection is still up
+    VPNHOST=$(ip route | grep default | grep tun | awk '{print $3}')
+    [ -n "$VPNHOST" ] || return 1
+    ping -c 1 -W 3 $VPNHOST > /dev/null 2>&1 || return 1
+    return 0
+}
+
+function checkCmd()
+{   
+    read -t 10 -N 1 CMD
+    if [ "$CMD" = "q" ]
+    then
+	echo
+	echo "Got 'q', disconnecting and exiting"
+	disconnect
+	exit 0
+    elif [ "$CMD" = "d" ]
+    then
+	echo
+	echo "Got 'd', disconnecting and waiting for 'q' or 'r'"
+	disconnect
+	WAIT="y"
+    elif [ "$CMD" = "r" ]
+    then
+	echo
+	echo "Got 'r', reconnecting"
+	disconnect
+	WAIT=""
+    elif [ "$CMD" = "p" ]
+    then
+	echo
+	ip addr show tun0
+	ip route | grep tun
+    fi 
+}
+
+WAIT=""
+function main()
+{
+    getCert
+    makeNcui
+    getDsid
+    echo "Starting vpn connect script"
+    echo "Hit q at anytime to quit, r to start/restart vpn, d to disconnect vpn"
+    while : 
+    do
+    	if  ! testNetwork
+	then
+	    echo "Waiting for network..."
+	    disconnect
+	elif ! testVpn
+	then
+	    echo "VPN connection failed or not connected, reconnecting"
+	    disconnect
+	    sleep 2
+	    connect
+	fi
+	checkCmd
+	while [ -n "$WAIT" ]
+	do
+	    checkCmd
+	done
+    done
+}
+
+main
