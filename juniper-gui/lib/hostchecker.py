090279b61c4b1310243ac48f9cba119587eb9a6c
Marshall Crocker <maroc81@users.noreply.github.com>
2017-01-28 09:16:57 -0600

    Moved host checker to separate file

---
+++ juniper-gui/lib/hostchecker.py
@@ -0,0 +1,113 @@
+import os
+import logging
+import subprocess, shlex
+import socket
+
+class HostChecker:
+    """
+    The HostChecker class starts, stops, and interfaces to the java host checker which 
+    is required for some juniper vpn connections.  The host checker is started with parameters
+    pulled from the sign in webpage and sent the preauth key.  The host checker connects
+    to the vpn and returns a response key that is used to complete the sign in.
+    """ 
+
+    defaultParams = {
+        'loglevel' : '2',
+        'postRetries' : '6',
+        'ivehost' : '',
+        'Parameter0' : '',locals
+        'locale' : 'en',
+        'home_dir' : os.path.expanduser('~'),
+        'user_agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'
+    }
+
+    def __init__(self, jar, narport):
+        self.jar = jar
+        self.narporttxt = narport
+        self.hcpid = None
+
+    def startHostChecker(self, params):
+        self.stopHostChecker()
+
+        # check params passed to host checker and use defaults for any not set
+        paramStr = ''
+        for param in HostChecker.defaultParams.keys():
+            if not param in params.keys():
+                paramStr = paramStr + param + ' "' + HostChecker.defaultParams[param] + '" '
+            else:
+                paramStr = paramStr + param + ' "' + params[param] + '" '
+
+        # remove old narport.txt file 
+        if os.path.exists(self.narporttxt):
+            os.remove(self.narporttxt)
+
+        # build the comand to start the host checker
+        cmd = 'java -classpath %s net.juniper.tnc.NARPlatform.linux.LinuxHttpNAR %s' % (self.jar, paramStr)
+        logging.debug('Staring host checker with cmd %s' % cmd)
+        cmd = shlex.split(cmd)
+        self.hcpid = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+        
+        # wait up to 10 seconds for narport.txt
+        for i in range(1, 10):
+            if os.path.exists(self.narporttxt): break
+            time.sleep(1)
+
+        # open narport and get port number for socket 
+        with open(self.narporttxt, 'r') as np:
+            self.port = int(np.read())
+            logging.debug('Got host checker port as %i' % self.port)
+
+    def stopHostChecker(self):
+        # first kill the process we have started
+        if not self.hcpid is None:
+            self.hcpid.poll()
+            if self.hcpid.returncode is None:
+                self.hcpid.terminate()
+                time.sleep(1)
+                self.hcpid.poll()
+                if self.hcpid.returncode is None:
+                    self.hcpid.kill()
+            else:
+                print self.hcpid.returncode
+        self.hcpid = None
+
+    def send(self, data, timeout=5):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+        sock.connect(('127.0.0.1', self.port))
+        logging.debug('Sending data to host checker %s' % data)
+        sock.sendall(data)
+        resp = sock.recv(2048)
+        sock.close()
+        logging.debug('Got response from host checker %s' % resp)
+        return resp
+
+    def doCheck(self, preauth, host):
+        """
+        Sends the running host checker the vpn site and the preauth cookie.
+        Expects the host checker to return a multiline response that starts with '200'
+        meaning the host check was successful. Returns the host check response
+        on success, raises an exception on failure.
+        """
+
+        data = 'start\nIC=%s\nCookie=%s\nDSSIGNIN=null\n' % (host, preauth)
+        resp = ""
+        try:
+            resp = self.send(data)
+        except socket.timeout:
+            # ignore socket timeout which is expected since recv buffer will not fill up entirely
+            print 'Got socket timeout exception, ignoring...'
+            pass
+        resp = resp.splitlines()
+        if len(resp) < 1:
+            raise Exception('No response from host checker')
+        if not '200' in resp[0]:
+            raise Exception('Invalid response from host checker %s' % resp[0])
+        return resp
+
+    def sendCookie(self, value):
+        try:
+            self.send('setcookie\nCookie=%s\n' % value, 2)
+        except socket.timeout:
+            # expect send function to timeout waiting for receive since host checker doesn't respond to this command
+            pass
